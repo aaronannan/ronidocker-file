a6549502370a0552fb8d906e0a7ecc63470df0b9
aaronannan <42162864+aaronannan@users.noreply.github.com>
2018-12-18 15:06:53 -0800

    created app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+FROM python
+WORKDIR /app
+ADD . /app
+RUN pip install -r requirements.txt
+EXPOSE 80
+ENV NAME world
+CMD [“python”, “app.py”]
+roni@docker-test:~/awsdocker$ cat app.py 
+from flask import Flask
+import os
+import socket
+app = Flask(__name__)
+@app.route(“/”)
+def hello():
+     html = “<h3>Hello {name}!</h3>” \
+            “<b>Hostname:</b> {hostname}<br/>”
+     return html.format(name=os.getenv(“NAME”, “world”), hostname=socket.gethostname())
+if __name__ == “__main__”:
+     app.run(host=’0.0.0.0', port=80)
+
